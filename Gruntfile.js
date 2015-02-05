58ba33029d80e9bc57a429aa332eb12c0a285bf2
unknown <UU074893@WS1371.ancor2.r.recruit.co.jp>
2015-02-05 19:53:08 +0900

    add compass, csslint, livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,63 @@
 module.exports = function(grunt) {
-  //config
-  grunt.initConfig({
-    connect: {
-      server: {
-        options: {base: 'resources/build'}
-      }
-    },
+    //config
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+          server: {
+            options: {base: 'resources/build'}
+          }
+        },
+        assemble: {
+            options: {
+                layoutdir: 'resources/src/layout',
+                data: ['resources/src/data/config.yml']
+            },
+            watch: {
+                expand: true,
+                cwd: 'resources/src',
+                src: '*.hbs',
+                dest: '<%= connect.server.options.base %>'
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            assemble: {
+                files: ['resources/src/**/*.hbs'],
+                tasks: ['assemble']
+            },
+            compass: {
+                files: ['resources/src/**/*.scss'],
+                tasks: ['compass', 'csslint', 'assemble']
+            },
+            data: {
+                files: ['resources/src/data/*.yml'],
+                tasks: ['assemble']
+            }
+        },
+        compass: {
+            dist: {
+                options: {
+                    sassDir:'resources/src/assets/scss',
+                    cssDir:'resources/build/assets/css'
+                }
+            }
+        },
+        csslint: {
+            check: {
+                src: 'build/css/*.css'
+            }
+        }
+    });
 
-    assemble: {
-      options: {layoutdir: 'resources/src/layout'},
-      watch: {
-        expand: true,
-        cwd: 'resources/src',
-        src: ['**/index.hbs'],
-        dest: '<%= connect.server.options.base %>'
-      }
-    },
+    //tasks
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('assemble');
 
-    watch: {
-      assemble: {
-        files: ['resources/src/**/*.hbs'],
-        tasks: 'assemble'
-      }
-    }
-  });
-
-  //tasks
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('assemble');
-
-  //regist
-  grunt.registerTask('default', ['connect', 'watch']);
+    //regist
+    grunt.registerTask('default', ['connect', 'watch']);
 };
