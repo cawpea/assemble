170e7ab8dff423c2298eaa26bcbee2a4caf1ae1b
unknown <UU074893@WS1371.ancor2.r.recruit.co.jp>
2015-02-06 18:12:19 +0900

    modified custom helper

--- resources/build/assets/js/custom-helper.js
+++ resources/build/assets/js/custom-helper.js
@@ -5,7 +5,7 @@ module.exports.hoge = function(str) {
 module.exports.myArray = function (array) {
     var result = [];
     for(var i=0, len = array.length;i < len;i++) {
-        result.push(array[i].title);
+        result.push('俺の' + array[i].title);
     }
     return result.join(', ');
 }
